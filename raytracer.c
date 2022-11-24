6dd1edb12fb5a50004fb3fc0ee5a4070f0a91b18
KyongRok <kyongrokk@gmail.com>
2022-11-24 15:09:00 -0500

    test

--- raytracer.c
+++ raytracer.c
@@ -342,6 +342,31 @@ int main(int argc , char* argv[]){
   // then , raytracer.exe testAmbient.txt
   // header file contains the struct
 
+  int Width = 128;	// Move these to your setup function. The actual resolution will be
+	int Height= 128;	// specified in the input file
+    char fname3[20] = "sceneP3.ppm"; //This should be set based on the input file
+	char fname6[20] = "sceneP6.ppm"; //This should be set based on the input file
+	unsigned char *pixels;
+	// This will be your image. Note that pixels[0] is the top left of the image and
+	// pixels[3*Width*Height-1] is the bottom right of the image.
+    unsigned char px[3*Width*Height];
+    pixels = px;
+
+	// This loop just creates a gradient for illustration purposes only. You will not use it.
+	float scale = 128.0 / (float) Width ;
+	int k = 0 ;
+	for(int i = 0; i < Height; i++) {
+		for (int j = 0; j < Width; j++) {
+			int c = (i+j)*scale ;
+			pixels[k] = c;
+			pixels[k+1] = c;
+			pixels[k+2] = c;
+			k = k + 3 ;
+		}
+	}
+	save_imageP3(Width, Height, fname3, pixels);
+	save_imageP6(Width, Height, fname6, pixels);
+
   return 0;
 
 }
@@ -379,5 +404,4 @@ int main() {
 	save_imageP3(Width, Height, fname3, pixels);
 	save_imageP6(Width, Height, fname6, pixels);
 }
-
 **/
